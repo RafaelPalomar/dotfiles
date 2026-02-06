3697a82f7c81c0d4961a25cd3efb5df0b50b83c7
Rafael Palomar <rafael.palomar@ous-research.no>
2026-02-06 13:39:10 +0100

    [ENH][systems/curie] Add `light` package
    This commit adds a package for increase/decrease screen brightness

--- entelequia/systems/curie.scm
+++ entelequia/systems/curie.scm
@@ -158,6 +158,7 @@
                                                     "grep"
                                                     "picom"
                                                     "direnv"
+                                                    "light"
                                                     "sed"))
                       %base-packages))
 
