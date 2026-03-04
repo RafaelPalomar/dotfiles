341a6ecf5f736081b65a051d480ea6d27ebe1cbc
Rafael Palomar <rafael.palomar@ous-research.no>
2026-03-04 13:35:18 +0100

    [ENH][profiles/development] Add AI agents to dev profile

--- entelequia/home/profiles/development.scm
+++ entelequia/home/profiles/development.scm
@@ -1,5 +1,7 @@
 (define-module (entelequia home profiles development)
   #:use-module (gnu packages)
+  #:use-module (entelequia packages aider)
+  #:use-module (entelequia packages claude-code)
   #:export (development-home-packages))
 
 ;;; Development home profile
@@ -8,27 +10,31 @@
 ;;; Includes compilers, build tools, and language-specific toolchains.
 
 (define development-home-packages
-  (map specification->package
-       '(;; C/C++ development
-         "gcc-toolchain"
-         "glibc"
-         "make"
-         "cmake"
+  (append
+   (map specification->package
+        '(;; C/C++ development
+          "gcc-toolchain"
+          "glibc"
+          "make"
+          "cmake"
 
-         ;; Node.js/JavaScript
-         "node"
+          ;; Node.js/JavaScript
+          "node"
 
-         ;; Python
-         "python"
-         "python-pip"
+          ;; Python
+          "python"
+          "python-pip"
 
-         ;; Debugging and profiling
-         "gdb"
-         "valgrind"
-         "strace"
+          ;; Debugging and profiling
+          "gdb"
+          "valgrind"
+          "strace"
 
-         ;; Build tools
-         "pkg-config"
-         "autoconf"
-         "automake"
-         "libtool")))
+          ;; Build tools
+          "pkg-config"
+          "autoconf"
+          "automake"
+          "libtool"))
+   ;; AI coding assistants (local packages)
+   (list claude-code
+         python-aider-chat)))
