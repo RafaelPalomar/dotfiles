f8a37469ac881730b46bab638e16022e1d2c6233
Rafael Palomar <rafael.palomar@ous-research.no>
2026-04-21 10:04:29 +0200

    [FIX] Correct parenthesis count in channels-lock.scm

--- channels-lock.scm
+++ channels-lock.scm
@@ -62,4 +62,4 @@
           (make-channel-introduction
             "0bbaf1fdd25266c7df790f65640aaa01e6d2dbc9"
             (openpgp-fingerprint
-              "8D10 60B9 6BB8 292E 829B  7249 AED4 1CC1 93B7 01E2")))))))
+              "8D10 60B9 6BB8 292E 829B  7249 AED4 1CC1 93B7 01E2"))))
